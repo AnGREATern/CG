b1cba2415e1251068fbd99010154c9c943cf735f
Romanov Vladislav <saromanovlad@gmail.com>
2024-05-21 17:40:58 +0300

    Add tests

---
+++ lab_04_01/unit_tests/test_geom.py
@@ -0,0 +1,20 @@
+import unittest
+import sys
+from PyQt5.QtCore import QPointF, QPoint
+from PyQt5.QtGui import QImage, QColor
+
+sys.path.append("./src")
+from algorithms import plot_pixel
+
+
+class TestGeom(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+
+    def testAddPoint(self):
+        res = plot_pixel(QImage(), QPointF(1.1, 245.2), QColor('r'))
+        self.assertEqual(res, QPoint(1, 245))
+
+
+if __name__ == "__main__":
+    unittest.main()
